3433bbe616cf5564b6908288bb1934930444a211
Inna Kolesnik <stutv@mail.ru>
2021-10-05 11:30:46 +0300

    homework2

--- src/Lesson2/MyArraySizeException.java
+++ src/Lesson2/MyArraySizeException.java
@@ -1,6 +1,6 @@
 package Lesson2;
 
-public class MyArraySizeException extends Exception{  // вызываемый метод
+public class MyArraySizeException extends Exception{  // вызываемый метод//
     //конструктор
     public MyArraySizeException (String mes){ //  сюда передали информацию в этом случае типа стринг,
         // мы их должны сохранить в область памяти message( можно писать любое имя переменной вместо message)
